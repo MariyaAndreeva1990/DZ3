4fabd3133a2c9ac3f7d1706141bd7b345cbb275b
Mariya Andreeva <mashanasha_1@example.cdotnet>
2022-11-27 17:57:59 +0600

    Add solution of Homework Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿int ReadInt(string message)//метод пользовательского ввода строки
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Ax = ReadInt("Введите координату x точки A: ");
+int Ay = ReadInt("Введите координату y точки A: ");
+int Az = ReadInt("Введите координату z точки A: ");
+
+int Bx = ReadInt("Введите координату x точки B: ");
+int By = ReadInt("Введите координату y точки B: ");
+int Bz = ReadInt("Введите координату z точки B: ");
+
+int sqrSideX = (Ax - Bx) * (Ax - Bx);
+int sqrSideY = (Ay - By) * (Ay - By);
+int sqrSideZ = (Az - Bz) * (Az - Bz);
+
+double distance = Math.Sqrt(sqrSideX + sqrSideY + sqrSideZ);
+Console.WriteLine($"Расстояние между двумя точками = {distance}");
