cc8ad455a747f93e85c8c779952f63632a6a7cb4
Mariya Andreeva <mashanasha_1@example.cdotnet>
2022-11-27 19:38:41 +0600

    Add solution of Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+int ReadInt(string message)//метод пользовательского ввода строки
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = ReadInt("Введите число: ");
+int count = 1;
+while (count <= number)
+{
+    Console.Write($"{count * count * count} ");
+    count++;
+}
