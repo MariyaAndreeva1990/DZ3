72c8df063e6d3001488a30dd06942f68f8a31def
Mariya Andreeva <mashanasha_1@example.cdotnet>
2022-11-28 11:30:43 +0600

    Add solution of Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да*/
+while (true)
+{
+int Read(string message)
+{
+Console.WriteLine(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = Read ("Введите пятизначное число: ");
+int number1 = number/10000;
+int number2 = (number/1000) % 10;
+int number3 = (number/10) % 10;
+int number4 = number % 10;
+int count = 0; 
+while (number > 0)
+{
+    number /= 10;
+    count++;
+}
+
+if (count != 5)
+{
+    Console.WriteLine("Введено не пятизначное число!");
+    continue;
+}
+if (number1 == number4 && number2 == number3)
+{
+     Console.Write("Число является палиндромом");
+}
+else
+{
+    Console.Write("Число не является палиндромом");
+}
+break;
+}
